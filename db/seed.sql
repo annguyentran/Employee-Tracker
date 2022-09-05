4346d63a5c947797a11f7740b06fe023274b1a84
Andrew Nguyen-Tran <ann013@ucsd.edu>
2022-09-05 15:47:38 -0700

    menu created

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,5 @@
 USE company_db;
 INSERT INTO departments(department_name) VALUES ("Sales"), ('Accounting'), ('engineer');
 INSERT INTO roles (title, salary, department_id) VALUES ("salesman",30000,1), ('Accountant',50000,2),('Engineer',50000,3);
-INSERT INTO employees (first_name, last_name, role_id,manager_id) VALUES ("Joe","smith",1,NULL),("JOHN","smith",2,NULL),("Alex","smith",3,NULL)
+INSERT INTO employees (id, first_name, last_name, role_id,manager_id) VALUES (1,"Joe","smith",1,NULL);
+INSERT INTO employees (first_name, last_name, role_id,manager_id) VALUES ("JOHN","smith",2,1),("Alex","smith",3,1)
--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 require('dotenv').config()
+const menu = () => {
+  inquirer.prompt(
+    [{
+      type: 'list',
+      message:"What would you like to do?",
+      name: 'firstMenu',
+      choices: ["VIEW ALL EMPLOYEES", "ADD EMPLOYEE","UPDATE EMPLOYEE ROLE", "VIEW ALL ROLES", 
+      "ADD ROLE","VIEW ALL DEPARTMENTS", "ADD DEPARTMENT"]
+    }]
+  ).then((menuChoice)=>{
+    switch(menuChoice.firstMenu){
+      case "VIEW ALL EMPLOYEES":{
+       
+        break; 
+      }
+      case "ADD EMPLOYEE":{
+
+        break;
+      }
+      case "UPDATE EMPLOYEE ROLE":{
+
+        break;
+      }
+      case "VIEW ALL ROLES":{
+
+        break;
+      }
+      case "ADD ROLE":{
+
+        break;
+      }
+      case "VIEW ALL DEPARTMENTS":{
+
+        break;
+      }
+      case "ADD DEPARTMENT":{
+
+        break;
+      }
+    }
+  });
+
+}
 const db = mysql.createConnection(
     {
       host: 'localhost',
@@ -12,11 +56,11 @@ const db = mysql.createConnection(
     console.log(`Connected to the classlist_db database.`)
   );
   db.promise().query("SELECT * FROM departments").then(([res]) => console.log(res))
+  menu();
+  // function(para1) {
 
-  function(para1) {
 
+  // }
+  // (para1, para2)=>{
 
-  }
-  (para1, para2)=>{
-
-  }
+  // }
